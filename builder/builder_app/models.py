891db169b482fe9194608fd20363a76a5d28fba4
Juan Manuel García <jmg.utn@gmail.com>
2011-12-11 23:55:16 -0300

    model updates

--- builder/builder_app/models.py
+++ builder/builder_app/models.py
@@ -12,3 +12,33 @@ class Project(models.Model):
     short_description = models.CharField(max_length=100)
     long_description = models.CharField(max_length=1000)
     
+    members = models.ManyToManyField(User, verbose_name="members", related_name="project_members")
+    followers = models.ManyToManyField(User, verbose_name="followers", related_name="project_followers")
+    
+
+class UserProfile(models.Model):
+    
+    user = models.OneToOneField(User)
+    
+    name = models.CharField(max_length=100)
+    last = models.CharField(max_length=100)
+    profession = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='images', null=True, blank=True)
+    bio = models.CharField(max_length=1000)
+    
+    website = models.CharField(max_length=255)
+    blog = models.CharField(max_length=255)
+
+
+class Comment(models.Model):
+    
+    text = models.CharField(max_length=512)
+    project = models.OneToOneField(Project)
+    user = models.OneToOneField(User)
+
+
+class Message(models.Model):
+    
+    sender = models.OneToOneField(User, related_name="message_sender")
+    receiver = models.OneToOneField(User, related_name="message_receiver")
+    text = models.CharField(max_length=1000)
--- builder/settings.py
+++ builder/settings.py
@@ -12,8 +12,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/juanma/DESARROLLO/builder/sqlite.db',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
